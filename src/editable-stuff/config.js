0c2b554b8e61baf473c8303e17e2c1a6fa3a8b73
phong1-1 <phong@bhavi.us>
2021-07-10 17:51:19 -0700

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -5,7 +5,7 @@ const navBar = {
 
 // Main Body SECTION
 const mainBody = {
-  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
+  gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1, #1ad7c0, #0cc948, #044a11, #062a45, #232626",
   firstName: "Peter",
   middleName: "\"Swaggy P\"",
   lastName: "Hong",
