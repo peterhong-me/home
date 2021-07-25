25887178a50dbd2e0c1283a306efb90a8a063f8b
phong1-1 <peterhongnike@gmail.com>
2021-07-24 18:05:07 -0700

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -131,7 +131,7 @@ const skills = {
 
 // GET IN TOUCH SECTION
 const getInTouch = {
-  show: true,
+  show: false,
   heading: "Let's Get In Touch!",
   message:
     "If you have any questions, or if you just want to say hi, please feel free to email me at",
