fadd242233521b3ed96009eeb13640bf906b2ffc
phong1-1 <peterhongnike@gmail.com>
2021-08-05 11:07:39 -0700

    Update config.js

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -9,7 +9,7 @@ const navBar = {
 const mainBody = {
   gradientColors: "#4484ce, #1ad7c0, #ff9b11, #9b59b6, #ff7f7f, #ecf0f1",
   firstName: "Peter",
-  middleName: "\"Swaggy P\"",
+  // middleName: "\"Swaggy P\"",
   lastName: "Hong",
   message: " | Researcher | Engineer | Entrepreneur | Writer | Programmer | ",
   icons: [
