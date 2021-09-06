4908bb15ed96d58827f0f396a9977d67720ebd8b
phong1-1 <peterhongnike@gmail.com>
2021-09-05 17:49:29 -0700

    commit

--- src/editable-stuff/config.js
+++ src/editable-stuff/config.js
@@ -76,7 +76,7 @@ const leadership = {
     "Throughout my high school career, I've been extremely grateful to have opportunities to lead my peers and make an impact on the greater San Diego community. In my role as co-organizer and director of sponsorships for CoastalHacks, I primarily worked on obtaining sponsorships from local organizations, larger companies, and other nonprofits in order to provide the most optimal experience for participants. As head basketball coach of multiple Master Sports youth basketball teams, I fostered a culture of inclusivity, collaboration, and dedication whether in practices or during games.",
   images: [
     {
-      img: require("../editable-stuff/peterhong.png"),
+      img: require("../editable-stuff/hackathonworkshoppic.png"),
       label: "Hackathon",
       paragraph: "Organizing technical workshops for participants"
     },
